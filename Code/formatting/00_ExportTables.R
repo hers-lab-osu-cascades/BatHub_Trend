abc95f9a29f6f1e3e24da4eac3705e6f4ea66921
Laura McMahon <126629110+lamcmahon@users.noreply.github.com>
2026-05-08 14:39:20 -0700

    add database names to paste argument to make it easier to adjust names, adjust annotations

--- Code/formatting/00_ExportTables.R
+++ Code/formatting/00_ExportTables.R
@@ -5,59 +5,62 @@
 # set user ID ------------------------------------------
 ## set your computer name 
 ## ex C:\Users\mcmahonl\Box\HERS_Working\Bats\DatabaseBE would be "mcmahonl"
-user <- "rosesar"             
+user <- "rosesar" 
+
+# set database names ------------------------------------
+SRC_dbname <- "PNW_BatHub_Database_20251004.accdb"
+SRC_CALLS_dbname <- "PNW_BatHub_Database_AcousticOutput_20251004.accdb"  
+SRC_CALLS2_dbname <- "PNW_BatHub_Database_AcousticOutput_2024_to_Present_20251004.accdb"  
 
 # create filepaths -------------------------------------
 SRC = paste0("C:\\Users\\", 
              user, 
-             "\\Box\\HERS_Working\\Bats\\DatabaseBE\\PNW_BatHub_Database_20251004.accdb")
+             "\\Box\\HERS_Working\\Bats\\DatabaseBE\\", 
+             SRC_dbname)
+
 SRC_CALLS = paste0("C:\\Users\\", 
                    user,
-                   "\\Box\\HERS_Working\\Bats\\DatabaseBE\\PNW_BatHub_Database_AcousticOutput_20251004.accdb")
-SRC_CALLS2 = paste0("C:\\Users\\", user, 
-                    "\\Box\\HERS_Working\\Bats\\DatabaseBE\\PNW_BatHub_Database_AcousticOutput_2024_to_Present_20251004.accdb")
+                   "\\Box\\HERS_Working\\Bats\\DatabaseBE\\", 
+                   SRC_CALLS_dbname)
 
+SRC_CALLS2 = paste0("C:\\Users\\", user, 
+                    "\\Box\\HERS_Working\\Bats\\DatabaseBE\\",
+                    SRC_CALLS2_dbname)
+
+# set output -----------------------------------------
 OUT = "DataRaw/tables"
 
-# Make sure output folder exists
-# mkdir -p "$OUT"
-# 
-# mdb-export "$SRC" tblDeployment    > "$OUT/tblDeployment.csv"
-# mdb-export "$SRC" tblPointLocation > "$OUT/tblPointLocation.csv"
-# mdb-export "$SRC" tblSite          > "$OUT/tblSite.csv"
-# mdb-export "$SRC" tluClutterType   > "$OUT/tluClutterType.csv"
-# mdb-export "$SRC" tluWaterBodyType > "$OUT/tluWaterBodyType.csv"
-# mdb-export "$SRC_CALLS" tblDeploymentDetection7 > "$OUT/calls_to_2024.csv"
-# mdb-export "$SRC_CALLS2" tblDeploymentDetection8 > "$OUT/calls_from_2024.csv"
+# open each database and pull tables -----------------
 
-## Converting above code to R-friendly language to pull from dbs
-
-# Open each database and pull tables -----------------
-
-### database1
+## database1
 src <- RODBC::odbcConnectAccess2007(SRC)
 
-  ## Read in Access Tables
+  ### read in Access Tables
   tblDeployment <- RODBC::sqlFetch(src,"tblDeployment")
   tblPointLocation <- RODBC::sqlFetch(src, "tblPointLocation")
   tblSite <- RODBC::sqlFetch(src, "tblSite")
   tluClutterType <- RODBC::sqlFetch(src, "tluClutterType")
   tluWaterBodyType <-RODBC::sqlFetch(src, "tluWaterBodyType")
 
+  ### close database
   RODBC::odbcClose(src)
 
-### database 2
+## database 2
 src_calls <- RODBC::odbcConnectAccess2007(SRC_CALLS)
 
+  ### read in Access tables
   tblcallsto2023 <- RODBC::sqlFetch(src_calls, "tblDeploymentDetection7")
   
+  ### close database
   RODBC::odbcClose(src_calls)
 
-### database 3
+## database 3
 src_calls2 <- RODBC::odbcConnectAccess2007(SRC_CALLS2)
-                                  
+    
+  ### read in Access tables                              
   tblcallsfrom2024 <- RODBC::sqlFetch(src_calls2, "tblDeploymentDetection8")
   
+  ### close database
   RODBC::odbcClose(src_calls2)
 
 # save to R files ------------------------------------
